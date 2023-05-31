b258b17c8d179be23aae2e67606429f7a9f28951
Jovan Cvetkov <j.cvetkov-int@jcvetkov-int.levi9.com>
2023-05-31 13:12:22 +0200

    fixed user details

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./user.css";
 
-function User({ id, name, desc, dep, del }) {
+function User({ id, name, desc, dep, del, setOpenUserDetails }) {
   return (
     <div
       className="user__div cursor-pointer"
-      onClick={() => props.setOpenUserDetails(true)}
+      onClick={() => setOpenUserDetails(true)}
     >
       <h3>{name}</h3>
       <p className="p__user__desc">{desc}</p>
